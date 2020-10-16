5c0592ef5cf8e80071f266e449b97fedc3c447bc
isadorateles <isadoratelesdecastro@gmail.com>
2020-10-16 14:42:02 +0200

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -15,19 +15,15 @@ var socket = io();
  * Connexion de l'utilisateur
  * Uniquement si le username n'est pas vide et n'existe pas encore
  */
-$('#login form').submit(function (e) 
-{
+$('#login form').submit(function (e) {
     e.preventDefault();
 
     blob.name = $('#login input').val().trim();
-    
-    if (blob.name.length > 0) 
-    { 
+
+    if (blob.name.length > 0) {
         // Si le champ de connexion n'est pas vide
-        socket.emit('user-login', blob, function (success) 
-        {
-            if (success) 
-            {
+        socket.emit('user-login', blob, function (success) {
+            if (success) {
                 $('body').removeAttr('id'); // Cache formulaire de connexion
                 $('#chat input').focus(); // Focus sur le champ du message
             }
@@ -38,36 +34,30 @@ $('#login form').submit(function (e)
 /**
  * Déconnexion d'un utilisateur
  */
-socket.on('user-logout', function (blob) 
-{
+socket.on('user-logout', function (blob) {
     var blobIndex = allBlobs.indexOf(blob);
-    if (blobIndex !== -1) 
-    {
+    if (blobIndex !== -1) {
         allBlobs.splice(blobIndex, 1);
     }
 
-}
+});
 
 /**
  * Réception d'un message
  */
-socket.on('allBlobs', function (allBlobs) 
-{
+socket.on('allBlobs', function (allBlobs) {
     allBlobsHere = allBlobs;
 });
 
-function setup()
-{
+function setup() {
 
 }
 
-function draw()
-{
+function draw() {
 
 }
 
-function mouseDragged()
-{
+function mouseDragged() {
     blob.draw();
 
     /**
